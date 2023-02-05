87d4b9b9c22a0f0373c080081026666be83c4032
SEMREDIN NUREGBA <semernur11@gmail.com>
2023-02-05 17:18:27 +0300

    Routes Fixed and register pages are added

--- entry/views.py
+++ entry/views.py
@@ -26,23 +26,6 @@ def registerPage(request):
     return render(request, 'base/login_register.html')
 
 
-def activate(request, uidb64, token):
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-
-    if user is not None and generate_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        messages.success(request, "Your Account has been activated!!")
-        return redirect('update-user')
-    else:
-        return render(request, 'activation_failed.html')
-
-
 def userProfile(request, pk):
     return render(request, 'base/profile.html')
 
