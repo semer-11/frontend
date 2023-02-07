6e37d7ead2b956e116754a9305b97c7d4f7de76e
SEMEREDIN NURGEBA <85545098+semer-11@users.noreply.github.com>
2023-02-07 21:28:09 +0300

    Delete entry directory

--- entry/forms.py
+++
@@ -1,9 +0,0 @@
-
-from django.forms import ModelForm
-from entry.models import Resident
-
-class ResidentForm(ModelForm):
-       class Meta:
-        model = Resident
-        fields = '__all__'
-        exclude = ['is_active','']
